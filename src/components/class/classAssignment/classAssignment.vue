41f208973227b856b9cc0bebe694758c99acecdc
Junyoung <tom9744@gmail.com>
2020-11-14 20:01:03 +0900

    Assignment & Management View

--- src/components/class/classAssignment/classAssignment.vue
+++ src/components/class/classAssignment/classAssignment.vue
@@ -1,7 +1,70 @@
-<template> <div>Assignment</div> </template>
+<template>
+  <v-container class="px-4 px-sm-6 px-md-8 px-xl-16">
+    <v-row>
+      <v-card elevation="0">
+        <v-col col="12">
+          <v-card
+            class="mb-7"
+            rounded="x1"
+            v-for="ass in assignment.reverse()"
+            :key="ass.name"
+          >
+            <v-card-subtitle
+              class="d-flex justify-space-between headline text--primary"
+            >
+              <div>{{ ass.name }}</div>
+              <div class=" red--text">
+                {{ ass.startdate }} ~ {{ ass.enddate }}
+              </div>
+            </v-card-subtitle>
+            <v-divider class="mx-4"></v-divider>
+            <v-card-text>{{ ass.explain }} </v-card-text>
+
+            <v-card-actions class="d-flex justify-end">
+              <v-btn class="blue lighten-3 white--text">제출 정보 보기</v-btn>
+              <v-btn class="blue lighten-3 white--text">수정하기</v-btn>
+              <v-btn class="red lighten-2 white--text">삭제하기</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-card>
+      <v-card-actions class=" text-center ml-auto">
+        <v-btn class="blue lighten-1 white--text" width="180px" height="50px">
+          <h3>+ 신규 과제 등록</h3>
+        </v-btn>
+      </v-card-actions>
+    </v-row>
+  </v-container>
+</template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    assignment: [
+      {
+        name: "6장 연습문제",
+        startdate: "2020.10.23",
+        enddate: "2020.10.30",
+        explain:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
+      },
+      {
+        name: "5장 연습문제",
+        startdate: "2020.10.05",
+        enddate: "2020.10.12",
+        explain:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
+      },
+      {
+        name: "4장 연습문제",
+        startdate: "2020.09.20",
+        enddate: "2020.10.01",
+        explain:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
+      }
+    ]
+  })
+};
 </script>
 
 <style></style>
--- src/components/class/classManage/classManage.vue
+++ src/components/class/classManage/classManage.vue
@@ -1,7 +1,150 @@
-<template> <div>Manage</div> </template>
+<template>
+  <v-container class="px-4 px-sm-6 px-md-8 px-xl-16">
+    <v-row>
+      <v-col cols="10" class="mb-6">
+        <p class="text-h4 font-weight-bold">{{ classInfo.title }}</p>
+        <p class="class-info text-h6 font-weight-bold">
+          {{ classInfo.teacher }}
+        </p>
+        <p class="text-subtitle-1">
+          {{ classInfo.time }}
+          <v-btn
+            class="mx-5 cyan darken-1 white--text"
+            width="90px"
+            height="25px"
+            rounded
+          >
+            시간 변경
+          </v-btn>
+        </p>
+        <p>
+          과목 코드 : {{ classInfo.code }}
+          <v-btn
+            class="mx-5 cyan darken-1 white--text"
+            width="80px"
+            height="25px"
+            rounded
+          >
+            재발급
+          </v-btn>
+        </p>
+      </v-col>
+      <v-col class="mt-10" cols="2">
+        <v-btn class="red lighten-1 white--text" elevation="10" x-large
+          >과목 삭제</v-btn
+        >
+      </v-col>
+
+      <v-col cols="6">
+        <div class="text-h4 font-weight-bold">수강생 목록</div>
+
+        <v-card rounded="xl">
+          <v-card-text
+            class="d-flex justify-space-between pa-2 align-center"
+            v-for="stu in studentList"
+            :key="stu.mail"
+          >
+            <div>{{ stu.name }} ( {{ stu.mail }} )</div>
+            <v-btn color="error" width="40px" height="25px" rounded>
+              삭제
+            </v-btn>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col cols="6">
+        <div class="text-h4 font-weight-bold">등록 대기자 목록</div>
+
+        <v-card rounded="xl">
+          <v-card-text
+            class="d-flex justify-space-between pl-2 py-0 align-center"
+            v-for="prestu in preStudentList"
+            :key="prestu.mail"
+          >
+            <div>{{ prestu.name }} ( {{ prestu.mail }} )</div>
+            <v-card-actions>
+              <v-btn
+                color="teal lighten-1 white--text"
+                width="40px"
+                height="25px"
+                rounded
+              >
+                수락
+              </v-btn>
+              <v-btn color="error" width="40px" height="25px" rounded>
+                거절
+              </v-btn>
+            </v-card-actions>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    classInfo: {
+      title: "물리",
+      teacher: "이정우 선생님",
+      time: "화요일 1교시, 수요일 3교시, 목요일 2교시, 금요일 7교시",
+      code: "dkssud32Wmt"
+    },
+    studentList: [
+      {
+        name: "이정우",
+        mail: "dwd4791@gmail.com"
+      },
+      {
+        name: "김정우",
+        mail: "dwd4792@gmail.com"
+      },
+      {
+        name: "정우",
+        mail: "dwd4793@gmail.com"
+      },
+      {
+        name: "우정",
+        mail: "dwd4794@gmail.com"
+      },
+      {
+        name: "정정우",
+        mail: "dwd4795@gmail.com"
+      }
+    ],
+    preStudentList: [
+      {
+        name: "정우",
+        mail: "dwd4796@gmail.com"
+      },
+      {
+        name: "우정",
+        mail: "dwd4797@gmail.com"
+      },
+      {
+        name: "정정우",
+        mail: "dwd4798@gmail.com"
+      },
+      {
+        name: "정정우",
+        mail: "dwd4799@gmail.com"
+      },
+      {
+        name: "정정정",
+        mail: "dwd4790@gmail.com"
+      }
+    ]
+  })
+};
 </script>
 
-<style></style>
+<style scoped>
+.class-info {
+  margin-bottom: 0px;
+}
+.card-text-title {
+  font-size: 20px;
+  margin-bottom: 0px;
+}
+</style>
