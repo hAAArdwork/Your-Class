d87b1e4af0652ed5d3adbf0487174dc1d3f00b44
Seulgi <drinkear00@gmail.com>
2020-11-23 13:47:19 +0900

    add AssignmentRegister.vue/ AssignmentRegisterInfo.vue / AssignmentWrite.vue/ Question.vue

--- src/components/class/classAssignment/classAssignment.vue
+++ src/components/class/classAssignment/classAssignment.vue
@@ -1,70 +1,85 @@
 <template>
-  <v-container class="px-4 px-sm-6 px-md-8 px-xl-16">
-    <v-row>
-      <v-card elevation="0">
-        <v-col col="12">
+  <v-container class="px-4 px-sm-6 px-md-8 px-xl-16" >
+      <v-toolbar elevation="0">
+         <v-spacer></v-spacer>
+        <v-toolbar-items>
+        <v-btn color="accent" fab small icon @click="$router.push({name: 'classAssignmentWrite'})">
+          <v-icon class="blue lighten-3 white--text">mdi-plus</v-icon>
+        </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+
+          <v-card >
           <v-card
             class="mb-7"
             rounded="x1"
-            v-for="ass in assignment.reverse()"
-            :key="ass.name"
+            v-for="ass in assignment"
+            :key="ass.number"
           >
             <v-card-subtitle
               class="d-flex justify-space-between headline text--primary"
             >
               <div>{{ ass.name }}</div>
-              <div class=" red--text">
-                {{ ass.startdate }} ~ {{ ass.enddate }}
+              <div class="red--text">
+                 ~ {{ ass.enddate }}
               </div>
             </v-card-subtitle>
             <v-divider class="mx-4"></v-divider>
-            <v-card-text>{{ ass.explain }} </v-card-text>
+            <v-card-text >{{ ass.explain }} </v-card-text>
 
             <v-card-actions class="d-flex justify-end">
-              <v-btn class="blue lighten-3 white--text">제출 정보 보기</v-btn>
-              <v-btn class="blue lighten-3 white--text">수정하기</v-btn>
-              <v-btn class="red lighten-2 white--text">삭제하기</v-btn>
+              <v-btn class="blue lighten-3 white--text" @click="$router.push({name: 'classAssignmentRegisterInfo'})">제출 확인</v-btn>
+              <v-btn class="red lighten-2 white--text" >
+                <v-icon left> mdi-delete</v-icon>
+                삭제하기</v-btn>
             </v-card-actions>
           </v-card>
-        </v-col>
-      </v-card>
-      <v-card-actions class=" text-center ml-auto">
-        <v-btn class="blue lighten-1 white--text" width="180px" height="50px">
-          <h3>+ 신규 과제 등록</h3>
-        </v-btn>
-      </v-card-actions>
-    </v-row>
+          </v-card>
+   
+
+
   </v-container>
+
+
+
+
+
 </template>
+ 
+
+
+
+
 
 <script>
 export default {
   data: () => ({
     assignment: [
       {
-        name: "6장 연습문제",
-        startdate: "2020.10.23",
-        enddate: "2020.10.30",
+        number: 1,
+        name: "4장 연습문제",
+        enddate: "2020.10.01",
         explain:
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
       },
       {
+        number: 2,
         name: "5장 연습문제",
-        startdate: "2020.10.05",
         enddate: "2020.10.12",
         explain:
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
       },
       {
-        name: "4장 연습문제",
-        startdate: "2020.09.20",
-        enddate: "2020.10.01",
+        number: 3,
+        name: "6장 연습문제",
+        enddate: "2020.10.30",
         explain:
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
-      }
+      },
     ]
   })
+
+  
 };
 </script>
 
-<style></style>
---
+++ src/components/class/classAssignment/classAssignmentRegister.vue
@@ -0,0 +1,52 @@
+<template> 
+  <v-container fluid>
+          <v-form>
+               <v-row>
+              <v-col cols="1"></v-col>
+              <v-col cols="10">
+                  <v-text-field v-model="title" label="제출명" ></v-text-field>
+                  <v-textarea v-model="comment" label="코멘트" no-resize rows="15" ></v-textarea>
+                  <v-file-input v-model="file" label="파일" show-size multiple counter></v-file-input>
+              </v-col> 
+          </v-row>
+          </v-form>
+
+          
+      <div class="text-center">
+          <v-btn class="mr-4 blue lighten-3 white--text" @click="$router.go(-1)">
+              <v-icon left>mdi-arrow-left</v-icon>
+              취소
+          </v-btn>
+          <v-btn class="mr-4 blue lighten-3 white--text" @click="modify">
+              <v-icon left> mdi-checkbox-marked-circle </v-icon>
+              수정
+          </v-btn>
+      </div>
+
+
+  </v-container>
+
+</template>
+
+
+
+<script>
+export default {
+
+    data: () => ({
+        title: '과제 제출합니다.',
+        comment: '안녕하세요 과제 제출합니다',
+        submitdate: "2020.10.27",
+        file: "cache.zip"
+    }),
+
+    methods: {
+        modify () {
+            
+        },
+    },
+    
+
+
+}
+</script>
